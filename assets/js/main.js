07e48c24e8ede2b6c7098fd3ddf09837a5c60b47
LesterGallagher <LesterGallagher@gmail.com>
2019-02-22 17:02:53 +0100

    IE fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,48 @@
-
-"use strict"; // Start of use strict
-var sideNav = document.getElementById('sideNav');
-var diagonalMessage = document.getElementById('diagonal-message');
-
-window.addEventListener('scroll', handleScroll);
-handleScroll();
-
-function handleScroll() {
-  if (sideNav) sideNav.style.height = Math.max(50, 150 - window.scrollY) + 'px';
-  if (diagonalMessage) diagonalMessage.style.top = Math.max(50, 150 - window.scrollY) + 42 + 'px';
-}
-
-// var msnry = new Masonry('.grid', {
-//   itemSelector: '.grid-item',
-//   columnWidth: 200
-// });
-
-const $$cookieDisclaimer = document.querySelector('.js-cookie-disclaimer');
-
-if (!localStorage.getItem('cookieDisclaimer')) {
-  $$cookieDisclaimer.classList.add('is-active');
-}
-
-$$cookieDisclaimer.querySelector('button').addEventListener('click', () => {
-  localStorage.setItem('cookieDisclaimer', true);
-  $$cookieDisclaimer.classList.remove('is-active');
-});
-
-if ('serviceWorker' in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using the default scope.
-  navigator.serviceWorker.register('/sw.js').then(function (registration) {
-    console.log('Service worker registration succeeded:', registration);
-  }, /*catch*/ function (error) {
-    console.log('Service worker registration failed:', error);
-  });
+if (document.readyState === "complete" || document.readyState === "loaded") {
+  handleLoaded();
 } else {
-  console.log('Service workers are not supported.');
+  document.addEventListener('DOMContentLoaded', handleLoaded);
 }
-AOS.init();
+function handleLoaded() {
+  "use strict"; // Start of use strict
+  var sideNav = document.getElementById('sideNav');
+  var diagonalMessage = document.getElementById('diagonal-message');
+
+  window.addEventListener('scroll', handleScroll);
+  handleScroll();
+
+  function handleScroll() {
+    if (sideNav) sideNav.style.height = Math.max(50, 150 - window.scrollY) + 'px';
+    if (diagonalMessage) diagonalMessage.style.top = Math.max(50, 150 - window.scrollY) + 42 + 'px';
+  }
+
+  // var msnry = new Masonry('.grid', {
+  //   itemSelector: '.grid-item',
+  //   columnWidth: 200
+  // });
+
+  const cookieDisclaimer = document.querySelector('.js-cookie-disclaimer');
+
+  if (!localStorage.getItem('cookieDisclaimer')) {
+    cookieDisclaimer.classList.add('is-active');
+  }
+
+  cookieDisclaimer.querySelector('button').addEventListener('click', function() {
+    localStorage.setItem('cookieDisclaimer', true);
+    cookieDisclaimer.classList.remove('is-active');
+  });
+
+  if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      console.log('Service worker registration succeeded:', registration);
+    }, /*catch*/ function (error) {
+      console.log('Service worker registration failed:', error);
+    });
+  } else {
+    console.log('Service workers are not supported.');
+  }
+  AOS.init();
+
+}
