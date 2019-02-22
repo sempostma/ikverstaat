d72d80b8067961afe1df547a064f7982c71c64fd
LesterGallagher <LesterGallagher@gmail.com>
2019-02-22 16:09:43 +0100

    various fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,41 @@
-(function () {
-  "use strict"; // Start of use strict
 
-  var sideNav = document.getElementById('sideNav');
-  var diagonalMessage = document.getElementById('diagonal-message');
+"use strict"; // Start of use strict
+var sideNav = document.getElementById('sideNav');
+var diagonalMessage = document.getElementById('diagonal-message');
 
-  window.addEventListener('scroll', handleScroll);
-  handleScroll();
+window.addEventListener('scroll', handleScroll);
+handleScroll();
 
-  function handleScroll() {
-    if (sideNav) sideNav.style.height = Math.max(50, 150 - window.scrollY) + 'px';
-    if (diagonalMessage) diagonalMessage.style.top = Math.max(50, 150 - window.scrollY) + 42 + 'px';
-  }
+function handleScroll() {
+  if (sideNav) sideNav.style.height = Math.max(50, 150 - window.scrollY) + 'px';
+  if (diagonalMessage) diagonalMessage.style.top = Math.max(50, 150 - window.scrollY) + 42 + 'px';
+}
 
-  // var msnry = new Masonry('.grid', {
-  //   itemSelector: '.grid-item',
-  //   columnWidth: 200
-  // });
+// var msnry = new Masonry('.grid', {
+//   itemSelector: '.grid-item',
+//   columnWidth: 200
+// });
 
-  const $$cookieDisclaimer = document.querySelector('.js-cookie-disclaimer');
+const $$cookieDisclaimer = document.querySelector('.js-cookie-disclaimer');
 
-  if (!localStorage.getItem('cookieDisclaimer')) {
-    $$cookieDisclaimer.classList.add('is-active');
-  }
+if (!localStorage.getItem('cookieDisclaimer')) {
+  $$cookieDisclaimer.classList.add('is-active');
+}
 
-  $$cookieDisclaimer.querySelector('button').addEventListener('click', () => {
-    localStorage.setItem('cookieDisclaimer', true);
-    $$cookieDisclaimer.classList.remove('is-active');
+$$cookieDisclaimer.querySelector('button').addEventListener('click', () => {
+  localStorage.setItem('cookieDisclaimer', true);
+  $$cookieDisclaimer.classList.remove('is-active');
+});
+
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js').then(function (registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }, /*catch*/ function (error) {
+    console.log('Service worker registration failed:', error);
   });
-
-  if ('serviceWorker' in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      console.log('Service worker registration succeeded:', registration);
-    }, /*catch*/ function (error) {
-      console.log('Service worker registration failed:', error);
-    });
-  } else {
-    console.log('Service workers are not supported.');
-  }
-  AOS.init();
-})();
+} else {
+  console.log('Service workers are not supported.');
+}
+AOS.init();
