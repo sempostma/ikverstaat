0e59eedb1576aeccaec94cf808a3bcf152899a26
LesterGallagher <LesterGallagher@gmail.com>
2019-02-22 17:50:42 +0100

    sw disabled

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,17 +32,17 @@ function handleLoaded() {
     cookieDisclaimer.classList.remove('is-active');
   });
 
-  if ('serviceWorker' in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      console.log('Service worker registration succeeded:', registration);
-    }, /*catch*/ function (error) {
-      console.log('Service worker registration failed:', error);
-    });
-  } else {
-    console.log('Service workers are not supported.');
-  }
+  // if ('serviceWorker' in navigator) {
+  //   // Register a service worker hosted at the root of the
+  //   // site using the default scope.
+  //   navigator.serviceWorker.register('/sw.js').then(function (registration) {
+  //     console.log('Service worker registration succeeded:', registration);
+  //   }, /*catch*/ function (error) {
+  //     console.log('Service worker registration failed:', error);
+  //   });
+  // } else {
+  //   console.log('Service workers are not supported.');
+  // }
   AOS.init();
 
 }
