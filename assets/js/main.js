17717ea967d0ac12ecc19dc3e9221501a0364c96
LesterGallagher <LesterGallagher@gmail.com>
2019-04-04 00:01:04 +0200

    various

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,14 +19,16 @@
 
   const cookieDisclaimer = document.querySelector('.js-cookie-disclaimer');
 
-  if (!localStorage.getItem('cookieDisclaimer')) {
-    cookieDisclaimer.classList.add('is-active');
-  }
+  if (cookieDisclaimer) {
+    if (!localStorage.getItem('cookieDisclaimer')) {
+      cookieDisclaimer.classList.add('is-active');
+    }
 
-  cookieDisclaimer.querySelector('button').addEventListener('click', function () {
-    localStorage.setItem('cookieDisclaimer', true);
-    cookieDisclaimer.classList.remove('is-active');
-  });
+    cookieDisclaimer.querySelector('button').addEventListener('click', function () {
+      localStorage.setItem('cookieDisclaimer', true);
+      cookieDisclaimer.classList.remove('is-active');
+    });
+  }
 
   // if ('serviceWorker' in navigator) {
   //   // Register a service worker hosted at the root of the
